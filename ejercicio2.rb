9162e4a63c7307b68ab9005b72b87a4a226a4244
Fernanda Granja <ferellen@MacBook-Pro-de-Fernanda.local>
2015-01-21 19:22:44 -0500

    hipotenusa

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -4,6 +4,6 @@ longitud1= gets.chomp.to_i
 puts "porfavor escribe la longitud del segundo lado del triangulo"
 longitud2= gets.chomp.to_i
 
-resultado= ((longitud1*2)+(longitud2*2))math.sqrt
+resultado= Math.sqrt((longitud1**2)+(longitud2**2))
 puts "la hipotenusa de tu triangulo es igual a #{resultado}"
 
